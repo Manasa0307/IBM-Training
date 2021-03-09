f355126622ab4b7decc2f6aaf5e27e555aecd318
BManasa <Manasa>
2021-03-09 17:10:27 +0530

    success

---
+++ Java_13/src/main/java/com/src/Java_13/App.java
@@ -0,0 +1,13 @@
+package com.src.Java_13;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
